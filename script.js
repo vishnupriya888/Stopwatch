0d51294c1d73541c57ee5997ae765917f0f794ce
vishnupriya-whitehatjr <59869563+vishnupriya-whitehatjr@users.noreply.github.com>
2021-08-23 16:12:21 +0530

    commit no milliseconds

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-let timerRef = document.querySelector('.timerDisplay');
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timerRef = document.querySelector(".timerDisplay");
 let int = null;
 
-document.getElementById('startTimer').addEventListener('click', ()=>{
-    if(int!==null){
-        clearInterval(int);
-    }
-    int = setInterval(displayTimer,10);
-});
-
-document.getElementById('pauseTimer').addEventListener('click', ()=>{
+document.getElementById("startTimer").addEventListener("click", () => {
+  if (int !== null) {
     clearInterval(int);
+  }
+  int = setInterval(displayTimer, 10);
+  console.log(int);
 });
 
-document.getElementById('resetTimer').addEventListener('click', ()=>{
-    clearInterval(int);
-    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+document.getElementById("pauseTimer").addEventListener("click", () => {
+  clearInterval(int);
 });
 
-function displayTimer(){
-    milliseconds+=10;
-    if(milliseconds == 1000){
-        milliseconds = 0;
-        seconds++;
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-            if(minutes == 60){
-                minutes = 0;
-                hours++;
-            }
-        }
+document.getElementById("resetTimer").addEventListener("click", () => {
+  clearInterval(int);
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+  timerRef.innerHTML = "00 : 00  ";
+});
+
+function displayTimer() {
+  milliseconds += 10;
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+      }
     }
-    let h = hours < 10 ? "0" + hours : hours;
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+  }
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms =
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
 
-    timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+  timerRef.innerHTML = `${m} : ${s}`;
 }
